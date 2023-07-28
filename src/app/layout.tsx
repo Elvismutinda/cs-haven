131cbcdfb864870e685334135a47f3563399bed0
Elvismutinda <elvocool@gmail.com>
2023-07-28 14:46:35 +0300

    Added basic home styling. To add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,35 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { cn } from "@/lib/utils";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'CS Haven',
-  description: 'A place for CS students to share their work and learn from others.'
-}
+  title: "CS Haven",
+  description:
+    "A place for CS students to share their work and learn from others.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        {/* <Navbar /> */}
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
