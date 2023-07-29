86ccf835c5d2f8691f78d7908e5fa50abb002e99
Elvismutinda <elvocool@gmail.com>
2023-07-29 16:16:13 +0300

    Updated Server session import

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import { buttonVariants } from "./ui/Button";
-import { getAuthSession } from "@/lib/auth";
 import { UserAccountNav } from "./UserAccountNav";
 import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
 const Navbar = async () => {
-  const session = await getAuthSession();
+  const session = await getServerSession(authOptions);
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
