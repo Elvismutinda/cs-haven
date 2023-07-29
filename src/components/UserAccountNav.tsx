d08a6ecc857e509fa8bc9c72cd8fa10eb8c2f524
Elvismutinda <elvocool@gmail.com>
2023-07-29 16:15:18 +0300

    Updated create community link

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -43,7 +43,7 @@ export function UserAccountNav({ user }: UserAccountNavProps) {
         </DropdownMenuItem>
 
         <DropdownMenuItem asChild>
-          <Link href="/r/create">Create Community</Link>
+          <Link href="/c/create">Create Community</Link>
         </DropdownMenuItem>
 
         <DropdownMenuItem asChild>
