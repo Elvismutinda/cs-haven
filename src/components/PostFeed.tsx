c2bad2c475c376b85aa17d4060d71ace3a353a9f
Elvismutinda <elvocool@gmail.com>
2023-07-31 15:03:04 +0300

    Fixed issue of wrapping UseSession in a session provider

--- src/components/PostFeed.tsx
+++ src/components/PostFeed.tsx
@@ -5,7 +5,9 @@ import { ExtendedPost } from "@/types/db";
 import { useIntersection } from "@mantine/hooks";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useSession } from "next-auth/react";
 import { useRef } from "react";
+import Post from "./Post";
 
 interface PostFeedProps {
   initialPosts: ExtendedPost[];
@@ -19,7 +21,9 @@ const PostFeed = ({ initialPosts, communityName }: PostFeedProps) => {
     threshold: 1,
   });
 
-  const {} = useInfiniteQuery(
+  const {data: session} = useSession();
+
+  const {data, fetchNextPage, isFetchingNextPage} = useInfiniteQuery(
     ["infinite-query"],
     async ({ pageParam = 1 }) => {
       const query =
@@ -38,11 +42,35 @@ const PostFeed = ({ initialPosts, communityName }: PostFeedProps) => {
   );
 
   const posts = data?.pages.flatMap((page) => page) ?? initialPosts;
+  
   return (
     <ul className="flex flex-col col-span-2 space-y-6">
-        {posts.map((post, index) => {
-            
-        })}
+      {posts.map((post, index) => {
+        const votesAmt = post.votes.reduce((acc, vote) => {
+          if (vote.type === "UP") {
+            return acc + 1;
+          }
+          if (vote.type === "DOWN") {
+            return acc - 1;
+          }
+          return acc;
+        }, 0);
+
+        const currenVote = post.votes.find(
+          (vote) => vote.userId === session?.user.id
+        );
+
+        if (index === posts.length - 1) {
+          return (
+            <li key={post.id} ref={ref}>
+              <Post />
+            </li>
+          );
+        } else {
+            return <Post />
+        }
+      })}
+
       <li></li>
     </ul>
   );
