55156afa966a6e486a767f61b3ef35178e8d92b9
Elvismutinda <elvocool@gmail.com>
2023-07-31 14:43:13 +0300

    Implementing posts to be shown in user feed

--- src/app/c/[slug]/page.tsx
+++ src/app/c/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import MiniCreatePost from "@/components/MiniCreatePost";
+import PostFeed from "@/components/PostFeed";
 import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/config";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
@@ -43,6 +44,7 @@ const page = async ({ params }: PageProps) => {
       </h1>
       <MiniCreatePost session={session} />
       {/* TODO: Show posts in user feed */}
+      <PostFeed />
     </>
   );
 };
