b49b0221cc378ee3102ee40bfe8b181f960d3b8d
Elvismutinda <elvocool@gmail.com>
2023-07-31 14:44:01 +0300

    Added PostFeed component to handle posts in the feed and infinite scrolling

---
+++ src/components/PostFeed.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/config";
+import { ExtendedPost } from "@/types/db";
+import { useIntersection } from "@mantine/hooks";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useRef } from "react";
+
+interface PostFeedProps {
+  initialPosts: ExtendedPost[];
+  communityName?: string;
+}
+
+const PostFeed = ({ initialPosts, communityName }: PostFeedProps) => {
+  const lastPostRef = useRef<HTMLElement>(null);
+  const { ref, entry } = useIntersection({
+    root: lastPostRef.current,
+    threshold: 1,
+  });
+
+  const {} = useInfiniteQuery(
+    ["infinite-query"],
+    async ({ pageParam = 1 }) => {
+      const query =
+        `/api/posts?limit=${INFINITE_SCROLL_PAGINATION_RESULTS}&page=${pageParam}` +
+        (!!communityName ? `&communityName=${communityName}` : "");
+
+      const { data } = await axios.get(query);
+      return data as ExtendedPost[];
+    },
+    {
+      getNextPageParam: (_, pages) => {
+        return pages.length + 1;
+      },
+      initialData: { pages: [initialPosts], pageParams: [1] },
+    }
+  );
+
+  const posts = data?.pages.flatMap((page) => page) ?? initialPosts;
+  return (
+    <ul className="flex flex-col col-span-2 space-y-6">
+        {posts.map((post, index) => {
+            
+        })}
+      <li></li>
+    </ul>
+  );
+};
+
+export default PostFeed;
