4c2610e1ef5693045e80823b724669cd9fb484d5
Elvismutinda <elvocool@gmail.com>
2023-07-31 09:03:47 +0300

    Added main page for community. Can see community info and user can subscribe or unsubscribe

---
+++ src/app/c/[slug]/layout.tsx
@@ -0,0 +1,119 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { notFound } from "next/navigation";
+import { format } from "date-fns";
+import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/Button";
+
+const Layout = async ({
+  children,
+  params: { slug },
+}: {
+  children: React.ReactNode;
+  params: { slug: string };
+}) => {
+  const session = await getAuthSession();
+
+  const community = await db.community.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+        },
+      },
+    },
+  });
+
+  const subscription = !session?.user
+    ? undefined
+    : await db.subscription.findFirst({
+        where: {
+          community: {
+            name: slug,
+          },
+          user: {
+            id: session.user.id,
+          },
+        },
+      });
+
+  const isSubscribed = !!subscription;
+
+  if (!community) {
+    return notFound();
+  }
+
+  const memberCount = await db.subscription.count({
+    where: {
+      community: {
+        name: slug,
+      },
+    },
+  });
+
+  return (
+    <div className="sm:container max-w-7xl mx-auto h-full pt-12">
+      <div>
+        {/* TODO: Button to take user back */}
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
+          <div className="flex flex-col col-span-2 space-y-6">{children}</div>
+
+          {/* Community Info sidebar */}
+          <div className="hidden md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
+            <div className="px-6 py-4">
+              <p className="font-semibold py-3">About c/{community.name}</p>
+            </div>
+
+            <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
+              <div className="flex justify-between gap-x-4 py-3">
+                <dt className="text-gray-500">Created</dt>
+                <dd className="text-gray-700">
+                  <time dateTime={community.createdAt.toDateString()}>
+                    {format(community.createdAt, "MMM d, yyyy")}
+                  </time>
+                </dd>
+              </div>
+
+              <div className="flex justify-between gap-x-4 py-3">
+                <dt className="text-gray-500">Members</dt>
+                <dd className="text-gray-700">
+                  <div className="text-gray-900">{memberCount}</div>
+                </dd>
+              </div>
+
+              {community.creatorId === session?.user?.id ? (
+                <div className="flex justify-between gap-x-4 py-3">
+                  <p className="text-gray-500">You created this community</p>
+                </div>
+              ) : null}
+
+              {community.creatorId !== session?.user?.id ? (
+                <SubscribeLeaveToggle
+                  communityId={community.id}
+                  communityName={community.name}
+                  isSubscribed={isSubscribed}
+                />
+              ) : null}
+
+              <Link
+              href={`c/${slug}/submit`}
+                className={buttonVariants({
+                  variant: "outline",
+                  className: "w-full mb-6",
+                })}
+              >
+                Create Post
+              </Link>
+            </dl>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
