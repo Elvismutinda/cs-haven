83c2f7be97d8924116e83349d3a2f1b514696985
Elvismutinda <elvocool@gmail.com>
2023-07-31 15:58:46 +0300

    Added Post content to be displayed

---
+++ src/components/Post.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { formatTimeToNow } from "@/lib/utils";
+import { Post, User, Vote } from "@prisma/client";
+import { MessageSquare } from "lucide-react";
+import Link from "next/link";
+import { useRef } from "react";
+import EditorOutput from "./EditorOutput";
+
+interface PostProps {
+  communityName: string;
+  post: Post & {
+    author: User;
+    votes: Vote[];
+  };
+  votesAmt: number;
+  commentAmt: number;
+}
+
+const Post = ({ communityName ,post, votesAmt: _votesAmt, commentAmt }: PostProps) => {
+  const pRef = useRef<HTMLParagraphElement>(null);
+  return (
+    <div className="rounded-md bg-white shadow">
+      <div className="px-6 py-4 flex justify-between">
+        {/* TODO: Post Votes */}
+        <div className="w-0 flex-1">
+          <div className="max-h-40 mt-1 text-xs text-gray-500">
+            {communityName ? (
+              <>
+                <a
+                  href={`/c/${communityName}`}
+                  className="underline text-zinc-900 text-sm underline-offset-2"
+                >
+                  c/{communityName}
+                </a>
+                <span className="px-1">•</span>
+              </>
+            ) : null}
+            <span>Posted by u/{post.author.name}</span>{" "}
+            {formatTimeToNow(new Date(post.createdAt))}
+          </div>
+
+          <a href={`/c/${communityName}/post/${post.id}`}>
+            <h1 className="text-lg font-semibold py-2 leading-6 text-gray-900">
+              {post.title}
+            </h1>
+          </a>
+
+          <div
+            className="relative text-sm max-h-40 w-full overflow-clip"
+            ref={pRef}
+          >
+            <EditorOutput content={post.content} />
+            {pRef.current?.clientHeight === 160 ? (
+              // blur bottom if content is too long
+              <div className="absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent"></div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-gray-50 z-20 text-sm px-4 py-4 sm:px-6">
+        <Link
+          href={`/c/${communityName}/post/${post.id}`}
+          className="w-fit flex items-center gap-2"
+        >
+          <MessageSquare className="h-4 w-4" /> {commentAmt} comments
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
