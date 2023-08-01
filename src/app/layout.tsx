3a27e4710e05e593d050292d0dccbb5538c776e5
Elvismutinda <elvocool@gmail.com>
2023-08-01 20:08:34 +0300

    Clean-ups

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import "./globals.css";
+import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
