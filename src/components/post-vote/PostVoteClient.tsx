5cf6935496432f29a38ba291c57a67e413738765
Elvismutinda <elvocool@gmail.com>
2023-08-01 11:53:25 +0300

    Added hooks and error handling for voting on client side

--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -9,21 +9,22 @@ import { usePrevious } from "@mantine/hooks";
 import { cn } from "@/lib/utils";
 import { useMutation } from "@tanstack/react-query";
 import { PostVoteRequest } from "@/lib/validators/vote";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { toast } from "@/hooks/use-toast";
 
 interface PostVoteClientProps {
   postId: string;
-  initialVoteAmt: number;
+  initialVotesAmt: number;
   initialVote?: VoteType | null;
 }
 
 const PostVoteClient = ({
   postId,
-  initialVoteAmt,
+  initialVotesAmt,
   initialVote,
 }: PostVoteClientProps) => {
   const { loginToast } = useCustomToasts();
-  const [voteAmt, setVoteAmt] = useState<number>(initialVoteAmt);
+  const [votesAmt, setVotesAmt] = useState<number>(initialVotesAmt);
   const [currentVote, setCurrentVote] = useState(initialVote);
   const prevVote = usePrevious(currentVote);
 
@@ -34,30 +35,83 @@ const PostVoteClient = ({
 
   const { mutate: vote } = useMutation({
     mutationFn: async (type: VoteType) => {
-        const payload: PostVoteRequest = {
-            postId: postId,
-            voteType: type,
-        }
+      const payload: PostVoteRequest = {
+        postId: postId,
+        voteType: type,
+      };
 
-        await axios.patch('/api/community/post/vote', payload)
-    }
-  })
+      await axios.patch("/api/community/post/vote", payload);
+    },
+    onError: (err, voteType) => {
+      if (voteType === "UP") setVotesAmt((prev) => prev - 1);
+      else setVotesAmt((prev) => prev + 1);
+
+      // Reset current vote
+      setCurrentVote(prevVote);
+
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast();
+        }
+      }
+
+      return toast({
+        title: "An error occurred.",
+        description: "Unable to vote. Please try again later.",
+        variant: "destructive",
+      });
+    },
+
+    onMutate: (type: VoteType) => {
+      if (currentVote === type) {
+        // user voting the same way again on same post
+        setCurrentVote(undefined);
+        if (type === "UP") setVotesAmt((prev) => prev - 1);
+        else if (type === "DOWN") setVotesAmt((prev) => prev + 1);
+      } else {
+        // user voting in opposite direction, subtract 2
+        setCurrentVote(type);
+        if (type === "UP") setVotesAmt((prev) => prev + (currentVote ? 2 : 1));
+        else if (type === "DOWN")
+          setVotesAmt((prev) => prev - (currentVote ? 2 : 1));
+      }
+    },
+  });
 
   return (
     <div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
       {/* Upvote */}
-      <Button size='sm' variant='ghost' aria-label="upvote">
-        <ArrowBigUp className={cn('h-5 w-5 text-zinc-700', {'text-emerald-500 fill-emerald-500': currentVote === 'UP',})} />
+      <Button
+        onClick={() => vote("UP")}
+        size="sm"
+        variant="ghost"
+        aria-label="upvote"
+      >
+        <ArrowBigUp
+          className={cn("h-5 w-5 text-zinc-700", {
+            "text-emerald-500 fill-emerald-500": currentVote === "UP",
+          })}
+        />
       </Button>
 
       {/* Vote count */}
       <p className="text-center py-2 font-medium text-sm text-zinc-900">
-        {voteAmt}
+        {votesAmt}
       </p>
 
       {/* Downvote */}
-      <Button size='sm' className={cn({'text-emerald-500': currentVote === 'DOWN',})} variant='ghost' aria-label="downvote">
-        <ArrowBigDown className={cn('h-5 w-5 text-zinc-700', {'text-red-500 fill-red-500': currentVote === 'DOWN'})} />
+      <Button
+        onClick={() => vote("DOWN")}
+        size="sm"
+        className={cn({ "text-emerald-500": currentVote === "DOWN" })}
+        variant="ghost"
+        aria-label="downvote"
+      >
+        <ArrowBigDown
+          className={cn("h-5 w-5 text-zinc-700", {
+            "text-red-500 fill-red-500": currentVote === "DOWN",
+          })}
+        />
       </Button>
     </div>
   );
