ea7613eb51d63addf80fa4f74a0fea8128507aff
Elvismutinda <elvocool@gmail.com>
2023-08-03 15:21:54 +0300

    Added api routes for comment features

---
+++ src/app/api/community/post/comment/route.ts
@@ -0,0 +1,38 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { CommentValidator } from "@/lib/validators/comment";
+import { z } from "zod";
+
+export async function PATCH(req: Request) {
+  try {
+    const body = await req.json();
+
+    const { postId, text, replyToId } = CommentValidator.parse(body);
+
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    // if no existing comment, create a new comment
+    await db.comment.create({
+      data: {
+        text,
+        postId,
+        authorId: session.user.id,
+        replyToId,
+      },
+    });
+
+    return new Response("OK");
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response("Invalid request data passed", { status: 422 });
+    }
+
+    return new Response("Could not create comment. Please try later", {
+      status: 500,
+    });
+  }
+}
