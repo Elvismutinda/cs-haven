9d00f6715f88ebf2fbc2fdbd8fd1f996647178f4
Elvismutinda <elvocool@gmail.com>
2023-08-04 15:15:10 +0300

    Added settings page and did some formatting

--- src/app/@authModal/(.)sign-in/page.tsx
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -1,12 +1,12 @@
-import CloseModal from '@/components/CloseModal'
-import SignIn from '@/components/SignIn'
+import CloseModal from "@/components/CloseModal";
+import SignIn from "@/components/SignIn";
 
 const page = () => {
   return (
-    <div className='fixed inset-0 bg-zinc-900/20 z-10'>
-      <div className='container flex items-center h-full max-w-lg mx-auto'>
-        <div className='relative bg-white w-full h-fit py-20 px-2 rounded-lg'>
-          <div className='absolute top-4 right-4'>
+    <div className="fixed inset-0 bg-zinc-900/20 z-10">
+      <div className="container flex items-center h-full max-w-lg mx-auto">
+        <div className="relative bg-white w-full h-fit py-20 px-2 rounded-lg">
+          <div className="absolute top-4 right-4">
             <CloseModal />
           </div>
 
@@ -14,7 +14,7 @@ const page = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
