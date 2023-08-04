e415b4edd00c9aef026247890bf8e65ea70a7b53
Elvismutinda <elvocool@gmail.com>
2023-08-04 15:42:05 +0300

    Added the username form

---
+++ src/components/UserNameForm.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import * as React from "react";
+import { toast } from "@/hooks/use-toast";
+import { UsernameRequest, UsernameValidator } from "@/lib/validators/username";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { User } from "@prisma/client";
+import axios, { AxiosError } from "axios";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/Card";
+import { Label } from "./ui/Label";
+import { Input } from "./ui/Input";
+import { Button } from "./ui/Button";
+import { cn } from "@/lib/utils";
+
+interface UserNameFormProps extends React.HTMLAttributes<HTMLFormElement> {
+  user: Pick<User, "id" | "username">;
+}
+
+const UserNameForm = ({ user, className, ...props }: UserNameFormProps) => {
+  const router = useRouter();
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<UsernameRequest>({
+    resolver: zodResolver(UsernameValidator),
+    defaultValues: {
+      name: user?.username || "",
+    },
+  });
+
+  const { mutate: updateUsername, isLoading } = useMutation({
+    mutationFn: async ({ name }: UsernameRequest) => {
+      const payload: UsernameRequest = { name };
+
+      const { data } = await axios.patch(`/api/username/`, payload);
+      return data;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 409) {
+          return toast({
+            title: "Username already taken.",
+            description: "Please choose another username.",
+            variant: "destructive",
+          });
+        }
+      }
+
+      return toast({
+        title: "Something went wrong.",
+        description: "Your username was not updated. Please try again.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: () => {
+      toast({
+        description: "Your username has been updated.",
+      });
+      router.refresh();
+    },
+  });
+
+  return (
+    <form
+      className={cn(className)}
+      onSubmit={handleSubmit((e) => updateUsername(e))}
+      {...props}
+    >
+      <Card>
+        <CardHeader>
+          <CardTitle>Your username</CardTitle>
+          <CardDescription>
+            Please enter your prefered display name
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="relative grid gap-1">
+            <div className="absolute top-0 left-0 w-8 h-10 grid place-items-center">
+              <span className="text-sm text-zinc-400">u/</span>
+            </div>
+            <Label htmlFor="name" className="sr-only">
+              Name
+            </Label>
+            <Input
+              id="name"
+              className="w-[400px] pl-6"
+              size={32}
+              {...register("name")}
+            />
+            {errors?.name && (
+              <p className="px-1 text-xs text-red-600">{errors.name.message}</p>
+            )}
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button isLoading={isLoading}>Change name</Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
+};
+
+export default UserNameForm;
