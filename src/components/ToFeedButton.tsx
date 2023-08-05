b5464f7c3b3fd74a4785324ae7eba2af7cc78b5e
Elvismutinda <elvocool@gmail.com>
2023-08-05 23:24:00 +0300

    Added button to take me back to feeed

---
+++ src/components/ToFeedButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { ChevronLeft } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { buttonVariants } from "./ui/Button";
+
+const ToFeedButton = () => {
+  const pathname = usePathname();
+
+  // if path is /c/mycom, turn into /
+  // if path is /c/mycom/post/cligad6jf0003uhest4qqkeco, turn into /c/mycom
+
+  const communityPath = getCommunityPath(pathname);
+
+  return (
+    <a href={communityPath} className={buttonVariants({ variant: "ghost" })}>
+      <ChevronLeft className="h-4 w-4 mr-1" />
+      {communityPath === "/" ? "Back home" : "Back to community"}
+    </a>
+  );
+};
+
+const getCommunityPath = (pathname: string) => {
+  const splitPath = pathname.split("/");
+
+  if (splitPath.length === 3) return "/";
+  else if (splitPath.length > 3) return `/${splitPath[1]}/${splitPath[2]}`;
+  // default path, in case pathname does not match expected format
+  else return "/";
+};
+
+export default ToFeedButton;
