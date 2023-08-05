cee5d621f1cf208a53d9ecc598685575c74b5ec0
Elvismutinda <elvocool@gmail.com>
2023-08-05 11:52:45 +0300

    Moved social links to create community card

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,13 +11,13 @@ const Navbar = async () => {
   return (
     <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
-          {/* Logo */}
-          <Link href="/" className="flex gap-2 items-center">
-            <Image src="/cs-haven.svg" width={43} height={43} alt="logo" />
-            <p className="hidden text-zinc-700 text-sm font-medium md:block">
-              CS HAVEN
-            </p>
-          </Link>
+        {/* Logo */}
+        <Link href="/" className="flex gap-2 items-center">
+          <Image src="/cs-haven.svg" width={43} height={43} alt="logo" />
+          <p className="hidden text-zinc-700 text-sm font-medium md:block">
+            CS HAVEN
+          </p>
+        </Link>
 
         {/* Search bar */}
         <SearchBar />
