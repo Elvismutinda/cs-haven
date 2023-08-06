6bfc5fffaf6ec51ed95f853dff86725650bf3aa2
Elvismutinda <elvocool@gmail.com>
2023-08-06 15:00:39 +0300

    Added github images for auth

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['lh3.googleusercontent.com', 'uploadthing.com']
+        domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com', 'uploadthing.com']
     },
     experimental: {
         appDir: true
