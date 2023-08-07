45f2ddb092dca7dc10af2097a0fd3048f43cd962
Elvismutinda <elvocool@gmail.com>
2023-08-07 12:32:35 +0300

    Modified github environment variables

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -22,8 +22,8 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
