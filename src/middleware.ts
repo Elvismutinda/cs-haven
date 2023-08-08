7ae5d3209889b99142412ada1f35141b9556b0c0
Elvismutinda <elvocool@gmail.com>
2023-08-08 11:21:05 +0300

    Added some paths that require authentication in the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,48 @@
-import { getToken } from 'next-auth/jwt'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
+// Middleware function to handle authentication and redirection
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req })
+  // Get the authentication token from the request
+  const token = await getToken({ req });
 
-  if (!token) {
-    return NextResponse.redirect(new URL('/sign-in', req.nextUrl))
+  // Paths that require authentication
+  const protectedPaths = [
+    "/c/:path*/submit",
+    "/c/create",
+    "/sign-in",
+    "/sign-up",
+  ];
+
+  // Check if the current path is an authentication page
+  const isAuthPage =
+    req.nextUrl.pathname.startsWith("/sign-in") ||
+    req.nextUrl.pathname.startsWith("/sign-up");
+
+  // If the current path requires authentication
+  if (protectedPaths.some((path) => req.nextUrl.pathname.startsWith(path))) {
+    // If user is not authenticated, redirect to sign-in page with original URL as parameter
+    if (!token) {
+      let from = req.nextUrl.pathname;
+      if (req.nextUrl.search) {
+        from += req.nextUrl.search;
+      }
+
+      return NextResponse.redirect(
+        new URL(`/sign-in?from=${encodeURIComponent(from)}`, req.url)
+      );
+    }
+  } else if (isAuthPage && token) {
+    // If user is authenticated and trying to access an authentication page, redirect to home page
+    return NextResponse.redirect(new URL("/", req.url));
   }
-}
 
+  // if (!token) {
+  //   return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
+  // }
+}
 
 export const config = {
-  matcher: ['/r/:path*/submit', '/r/create'],
-}
+  matcher: ["/c/:path*/submit", "/c/create", "/sign-in", "/sign-up"],
+};
