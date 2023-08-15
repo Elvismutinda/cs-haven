04019376d76bf35d94b5fa3bd50abd938a406c9e
Elvismutinda <elvocool@gmail.com>
2023-08-15 12:19:14 +0300

    Donate page loading skeleton

--- src/app/donate/loading.tsx
+++ src/app/donate/loading.tsx
@@ -4,15 +4,17 @@ import SectionShell from "@/components/SectionShell";
 
 const DonationLoading = () => {
   return (
-    <SectionShell>
-      <SectionHeading
-        heading="Donate to Creator"
-        text="If you're feeling very generous today, throw a coin or two my way. Thanks."
-      />
-      <div className="grid gap-10">
-        <CardSkeleton />
-      </div>
-    </SectionShell>
+    <div className="max-w-4xl mx-auto py-12">
+      <SectionShell>
+        <SectionHeading
+          heading="Donate to Creator"
+          text="If you're feeling very generous today, throw a coin or two my way. Thanks."
+        />
+        <div className="grid gap-10">
+          <CardSkeleton />
+        </div>
+      </SectionShell>
+    </div>
   );
 };
 
